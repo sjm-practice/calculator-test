b29fbf44dfd4c63e0cd83eba2a832610d38cec22
stevenjmarsh <sjm.atl@gmail.com>
2015-05-12 16:08:45 -0700

    first mocha browser tests

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # calculator-test
 This is a simple client side calculator, used for practicing testing client side javascript (with mocha, chai, sinon, karma).
+
+
+## Usage
+### To run mocha test via browser
+* run: python -m SimpleHTTPServer  (from project root directory)
+* in browser, go to http://localhost:8000/test
+* NOTE: if you open index.html as a file, blanket mocha will not load the source file successfully, and the coverage statics will be NaN
--- js/calculator.js
+++ js/calculator.js
@@ -1,1 +1,24 @@
-//
+function Calculator () {}
+
+Calculator.prototype._filter = function (values) {
+  var len = values.length,
+      filtered = values.filter(Number);
+
+  if (len !== filtered.length) {
+    throw new Error('All values must be numeric');
+  }
+
+  return values;
+};
+
+Calculator.prototype.add = function () {
+  var values = this._filter([].slice.apply(arguments));
+
+  if (values.length < 2) {
+    throw new Error('Add expects a minimum of two arguments but ' + values.length + ' were given.')
+  }
+
+  return values.reduce(function (a, b) {
+    return a + b;
+  });
+};
