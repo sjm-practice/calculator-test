06e4baff8200e253df9e27b937d0d440b23fbf03
stevenjmarsh <sjm.atl@gmail.com>
2015-05-12 14:24:40 -0700

    npm init and test index.html in place

---
+++ test/calculator-test.js
@@ -0,0 +1,21 @@
+
+describe('Calculator', function() {
+
+  before(function () {
+    this.calculator = new Calculator();
+  });
+
+  describe('Adding', function() {
+    it('should throw an error if a non-numeric value is used', function () {
+      expect(this.calculator.add.bind(this.calculator, 2, 'a')).to.throw();
+    });
+
+    it('should throw an error if less than 2 values are provided', function () {
+      expect(this.calculator.add.bind(this.calculator, 2)).to.throw();
+    });
+
+    it('should add a series of number arguments together', function () {
+      expect(this.calculator.add(2, 5)).to.equal(2 + 5);
+    });
+  });
+});
